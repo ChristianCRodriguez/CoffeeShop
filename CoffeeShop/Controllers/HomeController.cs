7827c7c52a64e9fa2fe180cfdf552d31bb3225b4
ChristianCRodriguez <christian@totallifechanges.com>
2020-01-23 21:56:32 -0500

    Added Register Page

---
+++ CoffeeShop/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using CoffeeShop.Models;
+using System.Text.RegularExpressions;
+
+namespace CoffeeShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View("~/Views/Account/Index.cshtml");
+        }
+
+        public IActionResult RegisterCustomer(CustomerModel customer)
+        {
+            return View("~/Views/Home/AuthorizedLanding.cshtml", customer);
+        }
+
+        public IActionResult AuthorizedLanding()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
